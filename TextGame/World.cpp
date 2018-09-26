b59534631db0ebcd64e0427c0f2371a8ce6dd32d
eaguirrezaba002 <eaguirrezaba001@gmail.com>
2018-09-26 16:38:08 +0200

    reading and writing the game world

--- TextGame/World.cpp
+++ TextGame/World.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "World.h"
+#include "Player.h"
 #include <iostream>
 #include <stdlib.h>
 #include <algorithm>
@@ -10,7 +11,35 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <istream>
+#include <vector>
 
+void World::ReadFile(std::string filename)
+{
+	cout << "Reading config file: " << filename << "\n";
+	ifstream inputFile;
+	char c1, c2;
+	inputFile.open(filename, fstream::in);
+	if (inputFile.is_open())
+	{
+		char c = ' ';
+		inputFile >> m_width >> c >> m_height >> c >> m_blank >> c >> m_coin >> c >> m_player1 >> c >> m_player2 >> c >> m_wall;
+		
+		string line;
+		
+		//load the maze into the vector;
+		for (int x = 0; x < m_height; x++) {
+			for (int y = 0; y < m_width; y++) {
+				inputFile >> c;
+				m_maze.push_back(c);
+			}
+			getline(inputFile, line);
+		}
+
+		inputFile.close();
+	}
+	else
+		cout << "Couldn't create file: " << filename;
+}
 
 World::World(std::string nameFile)
 {
@@ -21,6 +50,7 @@ World::World(std::string nameFile)
 
 	//TODO: initalize everything else
 	//...
+	ReadFile(nameFile);
 }
 
 
@@ -47,7 +77,27 @@ void World::drawMaze()
 	System::clear();
 
 	//TODO: -draw the maze: walls and each of the cells
+	//print the maze in the screen
+	cout << '|';
+	for (int x = 0; x < m_width; x++)
+		cout << "-";
 
+	cout << '|' << '\n' << '|';
+	for (int x = 0; x < (m_height*m_width); x++) {
+		//c = ;
+		if (x%m_width == 0 && x != 0)
+			cout << '|' << "\n" << '|';
+
+		if (m_maze.at(x) == m_blank)
+			cout << ' ';
+		else
+			cout << m_maze.at(x);
+	}
+
+	cout << '|' << "\n" << '|';
+	for (int x = 0; x < m_width; x++)
+		cout << "-";
+	cout << '|' << "\n";
 
 	//we sleep for a while
 	std::this_thread::sleep_for(std::chrono::milliseconds(100));
--- TextGame/World.h
+++ TextGame/World.h
@@ -2,19 +2,26 @@
 #include "Timer.h"
 #include "Player.h"
 #include <string>
+#include <vector>
 using namespace std;
 
 class World
 {
 	Timer m_timer;
-	Player m_player1;
-	Player m_player2;
+	Player player1;
+	Player player2;
+
+	int m_width = 0, m_height = 0;
+	char m_blank = ' ', m_coin = ' ', m_player1 = ' ', m_player2 = ' ', m_wall = ' ';
+	vector<char> m_maze;
 
 	void drawMaze();
 	
 public:
 	World(string nameFile);
 	~World();
+	void ReadFile(std::string filename);
+
 
 	void draw();
 };
