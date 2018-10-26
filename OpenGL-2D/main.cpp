f79864d20181e866029c9ddbef8089a93b8cdc9e
Denis <dstefanescu001@ikasle.ehu.eus>
2018-10-26 23:52:41 +0200

    Now the game stops when a player reaches 10 points and shows a message about who's the winner.

--- OpenGL-2D/main.cpp
+++ OpenGL-2D/main.cpp
@@ -7,13 +7,15 @@
 #include "Text.h"
 #include "Projectile.h"
 #include "Text.h"
+#include <iostream>
 #include "../3rd-party/freeglut3/include/GL/freeglut.h"
 #include "../3rd-party/SoundManager/SoundManager.h" //relative path to the main header
-
+using namespace std;
 
 
 int main(int argc, char** argv)
 {
+	int cont = 0;
 
 	//Sound
 	clock_t startSoundtrack = clock();
@@ -43,6 +45,7 @@ int main(int argc, char** argv)
 	}*/
 	Text2D *texto1 = new Text2D("PLAYER 1: 10", -0.75, 0.75, 1);
 	Text2D *texto2 = new Text2D("PLAYER 2: 10", 0.40, 0.75, 1);
+	Text2D *textWinner = new Text2D("GANADOR", -0.25, 0.5, 1);
 
 	renderer.addObject(texto1);
 	renderer.addObject(texto2);
@@ -73,21 +76,49 @@ int main(int argc, char** argv)
 
 	while (1)
 	{
-		secondsPassed = (clock() - startSoundtrack) / CLOCKS_PER_SEC;
-		if (secondsPassed > 80) {
-			startSoundtrack = clock();
-			pSoundManager->play(audioObj1, 1.f);
-		}
-		//UPDATE////////////////////
-		////////////////////////////
-		//process queued events
-		inputHandler.processEvents(pSoundManager, audioObj2);
-		glutMainLoopEvent();
+		if (inputHandler.doPlay() == true) {
 
-		//RENDER////////////////////
-		////////////////////////////
-		glutPostRedisplay();
-		glutSwapBuffers();
+			secondsPassed = (clock() - startSoundtrack) / CLOCKS_PER_SEC;
+			if (secondsPassed > 80) {
+				startSoundtrack = clock();
+				pSoundManager->play(audioObj1, 1.f);
+			}
+			//UPDATE////////////////////
+			////////////////////////////
+			//process queued events
+			inputHandler.processEvents(pSoundManager, audioObj2);
+			glutMainLoopEvent();
+
+			//RENDER////////////////////
+			////////////////////////////
+			glutPostRedisplay();
+			glutSwapBuffers();
+		}
+
+		if (pPlayer1->getScore() == 10) {
+			renderer.addObject(textWinner);
+			textWinner->setText("El jugador 1 ha ganado !!!");
+			inputHandler.stopPlaying();
+
+			inputHandler.processEvents(pSoundManager, audioObj2);
+			glutMainLoopEvent();
+
+			glutPostRedisplay();
+			glutSwapBuffers();
+		}
+		else if (pPlayer2->getScore() == 10)
+		{
+			renderer.addObject(textWinner);
+			textWinner->setText("El jugador 2 ha ganado !!!");
+			inputHandler.stopPlaying();
+
+			inputHandler.processEvents(pSoundManager, audioObj2);
+			glutMainLoopEvent();
+
+			glutPostRedisplay();
+			glutSwapBuffers();
+		}
+
 	}
    
 	return 0;
