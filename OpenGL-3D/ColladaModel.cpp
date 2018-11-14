7e66676f86ec6f01fc71ac9a7f790b1e464a2b46
denisro2010 <dead9mm@gmail.com>
2018-11-14 14:37:19 +0100

    ???

--- OpenGL-3D/ColladaModel.cpp
+++ OpenGL-3D/ColladaModel.cpp
@@ -1,14 +1,43 @@
 #include "ColladaModel.h"
 #include "../3rd-party/tinyxml2/tinyxml2.h"
-#include <vector>
+#include <vector>;
+#include "../3rd-party/SOIL/src/SOIL.h"
 
+using namespace std;
 
 ColladaModel::ColladaModel(const char* file)
 {
 	tinyxml2::XMLDocument doc;
 	doc.LoadFile(file);
-	tinyxml2::XMLElement* pRoot = doc.FirstChildElement(COLLADA);
-	tinyxml2::XMLElement* pLibraryImages = pRoot->FirstChildElement(library_images);
+	tinyxml2::XMLElement* pRoot = doc.FirstChildElement("COLLADA");
+
+	tinyxml2::XMLElement* pLibraryImages = pRoot->FirstChildElement("library_images");
+	tinyxml2::XMLElement* pImage = pLibraryImages->FirstChildElement("image");
+	tinyxml2::XMLElement* pInit_form = pImage->FirstChildElement("init_form");
+
+	const char* textureFile = pInit_form->GetText();
+	textureId = SOIL_load_OGL_texture(&textureFile[8], 0, 0, 0);
+
+	tinyxml2::XMLElement* pLibraryGeometries = pRoot->FirstChildElement("library_geometries");
+	tinyxml2::XMLElement* pGeometry = pLibraryGeometries->FirstChildElement("geometry");
+	tinyxml2::XMLElement* pMesh = pGeometry->FirstChildElement("mesh");
+
+	tinyxml2::XMLElement* pSource1 = pMesh->FirstChildElement("source");
+	tinyxml2::XMLElement* pFloat_array1 = pSource1->FirstChildElement("float_array");
+
+	tinyxml2::XMLElement* pSource2 = pSource1->NextSiblingElement("source");
+	tinyxml2::XMLElement* pFloat_array2 = pSource2->FirstChildElement("float_array");
+
+	tinyxml2::XMLElement* pSource3 = pSource2->NextSiblingElement("source");
+	tinyxml2::XMLElement* pFloat_array3 = pSource3->FirstChildElement("float_array");
+
+	tinyxml2::XMLElement* pTriangles = pRoot->FirstChildElement("library_geometries")->FirstChildElement("geometry")->FirstChildElement("mesh")->FirstChildElement("triangles");
+	tinyxml2::XMLElement* pP = pRoot->FirstChildElement("library_geometries")->FirstChildElement("geometry")->FirstChildElement("mesh")->FirstChildElement("triangles")->FirstChildElement("p");
+
+
+	parseXMLFloatArray(pFloat_array1, m_positions);
+
+
 }
 
 
@@ -20,7 +49,7 @@ void ColladaModel::draw() {
 
 }
 
-void ColladaModel::parseXMLFloatArray(tinyxml2::XMLElement *pFloatArray, std::vector<double> &vector)
+void ColladaModel::parseXMLFloatArray(tinyxml2::XMLElement *pFloatArray, vector<double> &vector)
 {
 	char* pCharArray = (char*)pFloatArray->GetText();
 	char* nextToken;
@@ -31,7 +60,7 @@ void ColladaModel::parseXMLFloatArray(tinyxml2::XMLElement *pFloatArray, std::ve
 		pt = strtok_s(0, " ", &nextToken);
 	}
 }
-void ColladaModel::parseXMLIntArray(tinyxml2::XMLElement *pFloatArray, std::vector<int> &vector)
+void ColladaModel::parseXMLIntArray(tinyxml2::XMLElement *pFloatArray, vector<int> &vector)
 {
 	char* pCharArray = (char*)pFloatArray->GetText();
 	char* nextToken;
@@ -42,4 +71,3 @@ void ColladaModel::parseXMLIntArray(tinyxml2::XMLElement *pFloatArray, std::vect
 		pt = strtok_s(0, " ", &nextToken);
 	}
 }
-
--- OpenGL-3D/ColladaModel.h
+++ OpenGL-3D/ColladaModel.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "GraphicObject3D.h"
 
+using namespace std;
+
 class ColladaModel :
 	public GraphicObject3D
 {
+	vector<double> m_positions;
+	vector<double> m_normals;
+	vector<double> m_texCoords;
+	int textureId;
+
 public:
-	ColladaModel(const char* file);
+	ColladaModel(const char * file);
 	~ColladaModel();
 	void draw();
-
-	void ColladaModel::parseXMLFloatArray(tinyxml2::XMLElement *pFloatArray, std::vector<double> & vector);
-	void ColladaModel::parseXMLIntArray(tinyxml2::XMLElement *pFloatArray, std::vector<int> &vector);
+	void parseXMLFloatArray(tinyxml2::XMLElement * pFloatArray, std::vector<double>& vector);
+	void parseXMLIntArray(tinyxml2::XMLElement * pFloatArray, std::vector<int>& vector);
 };
 
