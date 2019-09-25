b96ab6f12426a535dcb73cfbf403e82b6601dafb
Sergio Navarro <sernavarrops3@gmail.com>
2019-09-25 17:51:39 +0200

    Implementation Player class

--- TextGame/Player.cpp
+++ TextGame/Player.cpp
@@ -1,8 +1,13 @@
 #include "stdafx.h"
 #include "Player.h"
+#include "World.h"
 
-Player::Player()
+Player::Player(int x,int y, World pWorld)
 {
+	c_x=x;
+	c_y = y;
+	coins = 0;
+	world = pWorld;
 }
 
 
@@ -13,16 +18,65 @@ Player::~Player()
 
 void Player::moveUp()
 {
+	string option = world.attemptMove(c_x, c_y, c_x, c_y - 1);
+	if (option == "YES") {
+		c_y = c_y - 1;
+	}
+	else if (option == "COIN") {
+		c_y = c_y - 1;
+		addcoin();
+	}
 }
 
 void Player::moveDown()
 { 
+	string option = world.attemptMove(c_x, c_y, c_x, c_y + 1);
+	if (option == "YES") {
+		c_y = c_y + 1;
+	}
+	else if (option == "COIN") {
+		c_y = c_y + 1;
+		addcoin();
+	}
 }
 
 void Player::moveRight()
 {
+	string option = world.attemptMove(c_x, c_y, c_x + 1, c_y);
+	if (option == "YES") {
+		c_x = c_x + 1;
+	}
+	else if (option == "COIN") {
+		c_x = c_x + 1;
+		addcoin();
+	}
 }
 
 void Player::moveLeft()
 {
+	string option = world.attemptMove(c_x, c_y, c_x - 1, c_y);
+	if (option == "YES") {
+		c_x = c_x - 1;
+	}
+	else if (option == "COIN") {
+		c_x = c_x - 1;
+		addcoin();
+	}
+}
+
+int Player::getX()
+{
+	return c_x;
+}
+int Player::getY()
+{
+	return c_y;
+}
+int Player::getcoin()
+{
+	return coins;
+}
+void Player::addcoin()
+{
+	coins = coins++;
 }
